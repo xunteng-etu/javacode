42ae98eb148efe83c8580e7eece8a9dc723b6ede
lbr <364408616@qq.com>
2019-07-22 10:25:40 +0800

    1

---
+++ edu_parent/edu_parent/edu_auth_dubbo/src/main/java/com/edu/auth/dubbo/SpringbootApplication.java
@@ -0,0 +1,17 @@
+package com.edu.auth.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: edu_parent
+ * @description:
+ * @author: BaronLi
+ * @create: 2019-06-25 13:39
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+    public static void main (String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+}
